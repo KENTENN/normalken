bf5d30cefb42621189ea4b17d08cf7032f62b59b
KENN <157271873+KENTENN@users.noreply.github.com>
2025-12-14 12:54:59 +0700

    Add Core Init

--- Core/Init.lua
+++ Core/Init.lua
@@ -1,1 +1,16 @@
+-- รอเกมโหลดให้เสร็จก่อน (กัน error)
+repeat task.wait() until game:IsLoaded()
 
+-- โหลด UI
+local CreateUI = loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/KENTENN/normalken/main/UI/UI.lua"
+))()
+
+-- สร้างหน้าต่าง Hub
+local Window = CreateUI()
+
+-- ====== เลือกสคริปต์ตามเกม ======
+-- ตอนนี้โฟกัส Huntyzombie ก่อน
+loadstring(game:HttpGet(
+    "https://raw.githubusercontent.com/KENTENN/normalken/main/Games/Huntyzombie.lua"
+))(Window)
